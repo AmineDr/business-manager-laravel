522be41cc6d53ce94de896a1a1c5045634addcb6
Amine Droua <drouamine@gmail.com>
2024-03-27 06:53:11 +0000

    '_'

---
+++ init_backend.sh
@@ -0,0 +1,17 @@
+cd ./spark-lumen;
+echo 'APP_NAME=TravelList
+APP_ENV=production
+APP_KEY=
+APP_DEBUG=true
+APP_URL=http://localhost:8000
+
+LOG_CHANNEL=stack
+
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=business_manager
+DB_USERNAME=business_manager_user
+DB_PASSWORD=GDSlogin69' > .env;
+php composer.phar update;
+php composer.phar install --ignore-platform-reqs;
