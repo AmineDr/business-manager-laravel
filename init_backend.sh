7c254daff7b35c59998afb572f9b8e6ad24b2dda
Amine Droua <drouamine@gmail.com>
2024-03-27 09:16:02 +0000

    Deployment commit

--- init_backend.sh
+++ init_backend.sh
@@ -1,17 +1,17 @@
 cd ./spark-lumen;
-echo 'APP_NAME=TravelList
+echo 'APP_NAME=BusinessManager
 APP_ENV=production
 APP_KEY=
 APP_DEBUG=true
-APP_URL=http://localhost:8000
+APP_URL=http://localhost:8001
 
 LOG_CHANNEL=stack
 
 DB_CONNECTION=mysql
-DB_HOST=127.0.0.1
+DB_HOST=localhost
 DB_PORT=3306
 DB_DATABASE=business_manager
-DB_USERNAME=business_manager_user
+DB_USERNAME=bmuser
 DB_PASSWORD=GDSlogin69' > .env;
-php composer.phar update;
-php composer.phar install --ignore-platform-reqs;
+composer install --no-dev --optimize-autoloader;
+php artisan generate:key
