a68832166a04ccaec4efd11ad2a9884d22819121
Amine Droua <drouamine@gmail.com>
2024-03-27 09:27:41 +0000

    Deployment commit

--- spark-angular/src/app/common/axiosInstance.ts
+++ spark-angular/src/app/common/axiosInstance.ts
@@ -6,7 +6,7 @@ export class AxiosInstance extends Axios {
 
   constructor() {
     super({
-      baseURL: 'http://localhost:8000/api',
+      baseURL: 'http://localhost:8001/api',
       withCredentials: false,
       headers: {
         'Access-Control-Allow-Origin': '*',
