75ab4872be83bdbde226cf76df769b6f915716e5
Amine Droua <drouamine@gmail.com>
2024-03-27 05:49:49 +0000

    Update init_backend.sh

---
+++ init_backend.sh
@@ -0,0 +1,2 @@
+cd ./spark-lumen;
+echo "APP_NAME=TravelList\nAPP_ENV=production\nAPP_KEY=\nAPP_DEBUG=true\nAPP_URL=http://localhost:8000\n\nLOG_CHANNEL=stack\n\nDB_CONNECTION=mysql\nDB_HOST=127.0.0.1\nDB_PORT=3306\nDB_DATABASE=business_manager\nDB_USERNAME=business_manager_user\nDB_PASSWORD=GDSlogin69" > .env;
