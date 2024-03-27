d90aea428b73a1c2fa4bbfa93581a5f159866771
Amine Droua <drouamine@gmail.com>
2024-03-27 05:37:35 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-  name: "Business Manager CI/CD",
-  script: "",
-  watch: ".",
-
-  deploy: {
-    production: {
-      user: "root",
-      host: "155.133.22.183",
-      ref: "origin/master",
-      repo: "git@github.com:AmineDr/business-manager-laravel.git",
-      path: "/home/laravel_spark",
-      "pre-deploy-local": "./git_push.bat",
-      "post-deploy":
-        "pm2 reload ecosystem.config.js --env production && chmod +x ./init_backend.sh && chmod +x ./init_frontend.sh && ./init_backend.sh && ./init_frontend.sh",
-      "pre-setup": "",
-      ssh_options: "ForwardAgent=yes",
+  apps : [{
+    name: "Business Manager CI/CD",
+    scripts: {
+      start: ""
     },
-  },
+    watch: '.'
+  }
+],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '155.133.22.183',
+      ref  : 'origin/master',
+      repo : 'git@github.com:AmineDr/business-manager-laravel.git',
+      path : '/home/laravel_spark',
+      'pre-deploy-local': './git_push.bat',
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env production && chmod +x ./init_backend.sh && chmod +x ./init_frontend.sh && ./init_backend.sh && ./init_frontend.sh',
+      'pre-setup': '',
+      "ssh_options": "ForwardAgent=yes"
+    }
+  }
 };
